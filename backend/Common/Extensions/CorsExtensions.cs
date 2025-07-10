f898d3ac2ddfebdb701b9724efee174ffb2d8643
Ivan Fedorchenko <noncasted@gmail.com>
2025-07-10 06:37:46 +0500

    w

--- backend/Common/Extensions/CorsExtensions.cs
+++ backend/Common/Extensions/CorsExtensions.cs
@@ -14,11 +14,12 @@ public static class CorsExtensions
                 var url = GetUrl();
 
                 policy
-                    .SetIsOriginAllowed(origin =>
-                    {
-                        var uri = new Uri(origin);
-                        return uri.Host == "localhost" || uri.Host == "127.0.0.1";
-                    })
+                    .AllowAnyOrigin()
+                    // .SetIsOriginAllowed(origin =>
+                    // {
+                    //     var uri = new Uri(origin);
+                    //     return uri.Host == "localhost" || uri.Host == "127.0.0.1";
+                    // })
                     .AllowAnyMethod()
                     .AllowAnyHeader()
                     .AllowCredentials();
