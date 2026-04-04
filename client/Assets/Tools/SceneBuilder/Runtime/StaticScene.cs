571c76fb1e57bec33073e2c2ab213242f6d33032
Ivan Fedorchenko <65971171+noncasted@users.noreply.github.com>
2026-04-04 12:38:49 +0500

    [Client] Add SceneBuilder tool
    - add: SceneBuilder editor and runtime modules for codegen scene references
    - add: csproj DotSettings for SceneBuilder projects

---
+++ client/Assets/Tools/SceneBuilder/Runtime/StaticScene.cs
@@ -0,0 +1,16 @@
+using UnityEngine.AddressableAssets;
+
+namespace Tools.SceneBuilder
+{
+    public class StaticScene
+    {
+        private readonly AssetReference _reference;
+
+        public StaticScene(string guid)
+        {
+            _reference = new AssetReference(guid);
+        }
+
+        public AssetReference Value => _reference;
+    }
+}
